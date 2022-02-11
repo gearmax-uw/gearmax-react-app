3c418f567bab2322da11b498daf3e7372e6fae44
Casper Pang <c23pang@uwaterloo.ca>
2022-02-10 23:32:11 -0500

    test

--- src/pages/Search/index.tsx
+++ src/pages/Search/index.tsx
@@ -1,14 +1,23 @@
+//import { Card } from "antd";
 import { lazy } from "react";
-// import "./styles.css";;
+import "./styles.css";
+import CardContent from "../../content/CardContent.json";
 const Filter = lazy(() => import("../../components/Filter"));
-
+const CardGrid = lazy(() => import("../../components/SearchCard"));
 
 const Search = () => {
+  const cardNum = 50;
+  const CardList = Array.from({length : cardNum},  () => {return{CardAttr: CardContent}});
   return (
-<Filter>
-
-</Filter>
-
+    <div className="gridWrapper">
+      <div className="filterBox">
+          <Filter></Filter>
+       </div>
+      <div className="cardPageBox">
+          {<CardGrid cardNums={cardNum}  CardList={CardList}/>
+          }
+      </div>
+    </div>
   );
 };
 
